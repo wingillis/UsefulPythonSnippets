3fb7bad58d1d57db16df075cd7407eecd568cb15
Winthrop Gillis <win.gillis@gmail.com>
2014-05-12 14:37:09 -0400

    kwargs section

--- usefulPythonThings.py
+++ usefulPythonThings.py
@@ -1,3 +1,4 @@
+#!/Users/wgillis/envs/dataAnalysis/bin/python
 # Created by: Winthrop Gillis
 
 # This file will contain a conglomeration of useful python idioms
@@ -8,7 +9,7 @@ color= 'blue'
 
 print('The {speed} {color} fox'.format(**locals()))
 
-# Class attributes that I though you could only do in javascript
+# Class attributes that I thought you could only do in javascript
 class Modifiable():
 	pass
 
@@ -16,3 +17,16 @@ mod = Modifiable()
 mod.name = 'New Mod'
 mod.version = 1.0
 
+# You can actually store named args in a dictionary and unpack them for an argument
+def func(a, b, c):
+    print('{a}\n{b}\n{c}'.format(a=a,b=b,c=c))
+# OR
+def fun(**kwargs):
+    print('{a}\n{b}\n{c}'.format(**kwargs))
+
+keys = {'a': 'Lololol',
+        'b': 'do dwopp',
+        'c': 'helloooo'}
+
+func(**keys)
+fun(**keys)
